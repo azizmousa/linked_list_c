9337c7dfbebfe27fdb7b156d26df28cebee15186
azizmousa <azizmousa1010@gmail.com>
2020-12-18 16:29:31 +0200

    use the samrt pointers

--- include/linked_list/Node.hpp
+++ include/linked_list/Node.hpp
@@ -1,11 +1,13 @@
 #ifndef _NODE_HPP
 #define _NODE_HPP
 
+#include <memory>
+
 template<typename T>
 struct Node
 {
-    Node *next_ptr;
-    Node *previous_pre;
+    std::shared_ptr<Node> next_ptr;
+    std::shared_ptr<Node>previous_pre;
     T value;
 };
 
