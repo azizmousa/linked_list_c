8f2fa2ee9e39725c64f89ef57a96494b98a01f17
azizmousa <azizmousa1010@gmail.com>
2020-12-18 19:05:22 +0200

    initialize the pointers

--- include/linked_list/Node.hpp
+++ include/linked_list/Node.hpp
@@ -7,8 +7,15 @@ template<typename T>
 struct Node
 {
     std::shared_ptr<Node<T>> next_ptr;
-    std::shared_ptr<Node<T>>previous_pre;
+    std::shared_ptr<Node<T>> previous_ptr;
     T value;
+
+    Node(T value): next_ptr{nullptr}, previous_ptr{nullptr}, value{value}{
+        // std::cout << "Node: " << this << " created" << std::endl;
+    }
+    ~Node(){
+        // std::cout << "Node: " << this << " removed" << std::endl;
+    }
 };
 
 
