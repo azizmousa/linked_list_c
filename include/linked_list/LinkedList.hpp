5bc477657506314d38978f04927d1578c74c5236
azizmousa <azizmousa1010@gmail.com>
2020-12-18 16:40:40 +0200

    add linked list header

---
+++ include/linked_list/LinkedList.hpp
@@ -0,0 +1,37 @@
+#ifndef _LINKED_LIST_HPP
+#define _LINKED_LIST_HPP
+
+#include <memory>
+
+#include "linked_list/Node.hpp"
+
+template<typename T>
+class LikedList{
+
+private:
+
+    std::shared_ptr<Node> head; // the head of the list
+    std::shared_ptr<Node> tail; // the tail of the list
+    size_t size; // counter for the list size
+
+public:
+
+    LikedList() = default;
+    ~LikedList() = default;
+
+    // insert methods 
+    bool add_first(T value);
+    bool add_last(T value);
+
+    // remove methods
+    bool remove_first();
+    bool remove_last();
+    bool remove_val_first(T value);
+    bool remove_val_last(T value);
+
+    // display list values
+    void display();
+
+};
+
+#endif//_LIKED_LIST_HPP
