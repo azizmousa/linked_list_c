7ed2e00b1130235dd76a69600043bca2b9976a2d
azizmousa <azizmousa1010@gmail.com>
2020-12-19 21:56:58 +0200

    the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector>
+
 #include "linked_list/LinkedList.hpp"
 int main(int argc, char *argv[]){
+
     LinkedList<int> ls;
     ls.add_first(10);
     ls.add_first(30);
@@ -18,5 +21,12 @@ int main(int argc, char *argv[]){
     ls.display();
     std::cout << ls.get_size() << std::endl;
     std::cout << ls.find(10)->value << std::endl;
+    LinkedList<int>::NodePointer first = ls.begin();
+    LinkedList<int>::NodePointer last = ls.end();
+    std::cout << "First: " << first->value << std::endl;
+    std::cout << "Last: " << last->value << std::endl;
+    for(auto i{ls.begin()}; i != ls.end()->next_ptr; i=i->next_ptr){
+        std::cout << i->value << std::endl;
+    }
     return 0;
 }
